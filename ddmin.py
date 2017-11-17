c5367d1274dba6f9f1836cab1b485c7e3c26af49
Rahul Gopinath <rahul@gopinath.org>
2017-11-17 05:13:42 -0800

    fp

--- ddmin.py
+++ ddmin.py
@@ -1,17 +1,19 @@
 import random
 import string
+
+random.seed(0)
 def test(s):
     print("%s %d" % (s, len(s)))
     return set('()') <= set(s)
 
+def complement(s, i, l):
+    return s[:i] + s[i + l:]
+
 def some_complement_is_failing(s, n, testfn):
-    start = 0
     subset_length = len(s) // n
-    while start < len(s):
-        complement = s[:start] + s[start + subset_length:]
-        if testfn(complement): return complement
-        start += subset_length
-    return None
+    items = range(0,len(s), subset_length)
+    complements = (complement(s, i, subset_length) for i in items)
+    return next((i for i in complements if testfn(i)), None)
 
 
 def ddmin(s, fn):
